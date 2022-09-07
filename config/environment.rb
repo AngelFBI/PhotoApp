76565d425d73256cbd2cbe4258d996948176328c
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-07 16:10:03 -0500

    Added addon mailgun to test emails
    - Added enviroment variables in credentials
    - Added ActionMailer setting to send mails
    - Set mailer config in development.rb
    - Set mailer config in production.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :port           => Rails.application.credentials.mail[:port],
+  :address        => Rails.application.credentials.mail[:server],
+  :user_name      => Rails.application.credentials.mail[:login],
+  :password       => Rails.application.credentials.mail[:password],
+  :domain         => 'angelrails-photo-app.herokuapp.com',
+  :authentication => :plain,
+}
+ActionMailer::Base.delivery_method = :smtp
