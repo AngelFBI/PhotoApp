d466b7d2ccfdfc9116781bd2eee2b3310d4095e9
AngelFBI <angel.bejarano@enroutesystems.com>
2022-09-07 16:33:36 -0500

    Fixes for production

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,24 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :port           => Rails.application.credentials.mail[:port],
-  :address        => Rails.application.credentials.mail[:server],
-  :user_name      => Rails.application.credentials.mail[:login],
-  :password       => Rails.application.credentials.mail[:password],
-  :domain         => 'angelrails-photo-app.herokuapp.com',
-  :authentication => :plain,
-}
+if Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'angelrails-photo-app.herokuapp.com',
+    :authentication => :plain,
+  }
+else
+  ActionMailer::Base.smtp_settings = {
+    :port           => Rails.application.credentials.mail[:port],
+    :address        => Rails.application.credentials.mail[:server],
+    :user_name      => Rails.application.credentials.mail[:login],
+    :password       => Rails.application.credentials.mail[:password],
+    :domain         => 'angelrails-photo-app.herokuapp.com',
+    :authentication => :plain,
+  }
+end
+
 ActionMailer::Base.delivery_method = :smtp
